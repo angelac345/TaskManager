f44ce34ed913d612949c0db35617a61c709fa0f0
angelac345 <angelachao345@gmail.com>
2024-01-11 04:02:54 -0800

    taking into consideration all 3 fields

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function addTask() {
-    const taskInput = document.getElementById('task-input');
+    const taskName = document.getElementById('task-input');
+    const taskDeadline = document.getElementById('task-input-deadline');
+    const taskTimeEstimate = document.getElementById('task-input-estimated-time');
+
     const taskList = document.getElementById('task-list');
 
-    if (taskInput.value !== '') {
+    if (taskName.value !== '') {
         // Create a new list item
         const newTask = document.createElement('li');
-        newTask.textContent = taskInput.value;
+        newTask.textContent = taskName.value + ", " + taskDeadline.value + ", " + taskTimeEstimate.value;
 
         // Add the new task to the list
         taskList.appendChild(newTask);
